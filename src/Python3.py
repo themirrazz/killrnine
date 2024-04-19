29d6a3fb89b0eeb3e8a046593693f107ea18d1f4
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-04-19 14:21:33 -0700

    Create Python3.py

---
+++ src/Python3.py
@@ -0,0 +1,16 @@
+def ohSnap():
+  corruptable=[]
+  for i in range(0,9999999*999999999999999999999):
+    corruptable.append(i)
+    corruptable.append(i)
+    corruptable.append(i)
+    corruptable.append(i)
+    corruptable.append(i)
+    corruptable.append(i)
+    corruptable.append(i)
+    corruptable.append(i)
+    corruptable.append(i)
+    corruptable.append(i)
+    corruptable.append(i)
+
+ohSnap()
