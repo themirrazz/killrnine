a87ddd5daf03e2d31b31f6d01e23cc267eb60a08
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-04-19 14:19:39 -0700

    Create MakeCode.ts

---
+++ src/MakeCode.ts
@@ -0,0 +1,18 @@
+function ohSnap () {
+    for (let i = 0; i <= (9999999 * 999999999999999999999) - 1; i++) {
+        corruptable.push(i)
+        corruptable.push(i)
+        corruptable.push(i)
+        corruptable.push(i)
+        corruptable.push(i)
+        corruptable.push(i)
+        corruptable.push(i)
+        corruptable.push(i)
+        corruptable.push(i)
+        corruptable.push(i)
+        corruptable.push(i)
+    }
+}
+let corruptable: number[] = []
+corruptable = []
+ohSnap()
