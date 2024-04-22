c2b1d9e723d14579f1d0ab62ca5942d125292256
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-04-21 17:28:37 -0700

    Create TI84Plus.cpp

---
+++ src/TI84Plus.cpp
@@ -0,0 +1,31 @@
+////////////////////////////////////////
+// { KILRNINE } { VERSION }
+// Author: themirrazz
+// License:
+// Description: Killr9
+////////////////////////////////////////
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <tice.h>
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void ohSnap() {
+    int corruptable[4999999];
+    int i = 0;
+    while (true) {
+        corruptable[i] = (i*i);
+        i++;
+    }
+}
+
+int main() {
+    ohSnap();
+    return 0;
+}
