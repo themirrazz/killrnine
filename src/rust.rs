4aeb17d6ee3798ef5e77631a201dd067bf9502c3
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-12-03 23:58:40 -0700

    double fixed

--- src/rust.rs
+++ src/rust.rs
@@ -6,7 +6,7 @@ func ohSnap() {
     corruptable := []int{}
     i := 0
     for {
-        for j := 0; true; j++ {
+        for j := 0; j<10; j++ {
             corruptable = append(corruptable, i)
         }
         i++
